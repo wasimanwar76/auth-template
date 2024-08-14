15b8442ac7a797cf488e63912052e1ce75ba0a95
Wasim Anwar <wasimsonu76@gmail.com>
2024-08-14 16:16:45 +0530

    add global error handler

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send("<h1>Hello, Welcome to auth service</h1>");
+});
+
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(error);
+  const statusCode = error.statusCode || 500;
+  res.status(statusCode).json({
+    error: [
+      {
+        type: error.name,
+        message: error.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 export default app;
